2133ad20e34ef9d07e6bc82b05dc3ca1260e04a9
darknesspaladin <37006242+darknesspaladin@users.noreply.github.com>
2019-03-04 06:58:19 +0800

    2019/3/3 夜

---
+++ CPrimer Plus 练习/PE_3_2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int char_1 = 0;
+	
+	printf("ÊäÈëÊı×Ö£º");
+	scanf("%d",&char_1);
+	printf("%c",char_1);
+	
+	return 0;
+}
--- 字符串学习.c
+++ 字符串学习.c
@@ -1,4 +1,4 @@
-//如果编译器不识别%zd，尝试换成%u或%lu。
+/*//如果编译器不识别%zd，尝试换成%u或%lu。
 #include <stdio.h>
 #include <string.h>
 #define PRAISE "You are an extraordinary being."
@@ -9,11 +9,35 @@ int main(void)
 	printf("whta's your name?");
 	scanf("%s",name);
 	printf("Hello,%s.%s\n",name,PRAISE);
-	printf("Your name of %d letters occupies %d memory cells.\n",
+	printf("Your name of %u letters occupies %u memory cells.\n",
 		strlen(name),sizeof name);
-	printf("The phrase of praise has %d letters",
+	printf("The phrase of praise has %u letters",
 		strlen(PRAISE));
-	printf("and occupies %d memory cells.\n",sizeof PRAISE);
+	printf("and occupies %u memory cells.\n",sizeof PRAISE);
 	
 	return 0; 
-} 
+}*/
+/*//一些浮点型修饰符的组合
+#include <stdio.h>
+
+int main(void)
+
+{
+	const double RENT = 3852.99; // const 变量
+	
+	printf("*%f*\n",RENT); 
+	printf("*%e*\n",RENT);
+	printf("*%4.2f*\n",RENT);
+	printf("*%3.1f*\n",RENT);
+	printf("*%10.3f*\n",RENT);
+	printf("*%10.3E*\n",RENT);
+	printf("*%+4.2f*\n",RENT);
+	printf("*%010.2f*\n",RENT);
+	
+	return 0;
+}*/
+//演示一些格式标记
+#include <stdio>
+
+int main(void)
+ 
