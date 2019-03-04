f896928e7c54137a6608e0eff1700d2e196644ba
darknesspaladin <37006242+darknesspaladin@users.noreply.github.com>
2019-03-05 06:42:53 +0800

    2019/3/4 night

--- 字符串学习.c
+++ 字符串学习.c
@@ -36,8 +36,29 @@ int main(void)
 	
 	return 0;
 }*/
-//演示一些格式标记
-#include <stdio>
+/*//演示一些格式标记
+#include <stdio.h>
 
 int main(void)
+{
+	printf("%x %X %#x\n", 31, 31, 31);
+	printf("**%d**% d**% d**\n", 42, 42, -42);
+	printf("**%5d**%5.3d**%05d**%05.3d**\n", 6, 6, 6, 6);
+	
+	return 0;
+}*/
+//字符串格式
+#include <stdio.h>
+#define BLURB "Authentic imiation!"
+
+int main(void)
+
+{
+	printf("[%2s]\n",BLURB);
+	printf("[%24s]\n",BLURB);
+	printf("[%24.5s]\n",BLURB);
+	printf("[%-24.5s]\n",BLURB);
+	
+	return 0;
+ } 
  
