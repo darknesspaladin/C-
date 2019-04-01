eb74ebb61351c083a0e59be2c05f208f1592b5aa
darknesspaladin <37006242+darknesspaladin@users.noreply.github.com>
2019-04-02 05:44:29 +0800

    2019/4/2 zao

---
+++ 统计单词.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdbool.h>
+#define STOP '|'
+int main(void)
+{
+	char c; //读入字符
+	char prev; //读入的前一个字符
+	long n_chars = 0L; //字符数
+	int n_lines = 0; //行数
+	int n_words = 0; //单词数
+	int p_lines = 0; //不完整的行数
+	bool inword = false; //如果在单词c中，inword == true
+	
+	printf("Enter text to be analyzed (| to terminate):\n");
+	prev = '\n'
+	 
+} 
